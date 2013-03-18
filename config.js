e521aa55d5a34f8058584498d14396908fc15cac
KienBui <doankien.bui@gmail.com>
2013-03-19 00:56:40 +0700

    layout

---
+++ config.js
@@ -0,0 +1,18 @@
+// dr.topBar.height = 50;
+
+// //space = 10
+
+// dr.quizCard.width = 180
+// dr.quizCard.height = 130;
+
+// dr.quizCard.changeButton.width = 20;
+// dr.quizCard.changeButton.height = 20;
+
+// //space = 10
+
+// dr.funcButtonsBar.height = 60;
+
+// //scpace = 10
+
+// dr.board.width  = 240;
+// dr.board.height = 160;
--- dr.js
+++ dr.js
@@ -22,36 +22,81 @@ goog.require('dr.Sprite');
 // entrypoint
 dr.start = function(){
     //director
-  dr.director = new lime.Director(document.body,320,480);
+  dr.director = new lime.Director(document.body);
   dr.director.makeMobileWebAppCapable();
 
   var gamescene = new lime.Scene;
 
-  var layer = new lime.Layer();
-  var board = new dr.Board(320,220);
-  var submitbtn = dr.makeGlossyButton('submit').setPosition(160,280);
-  var clearbtn = dr.makeGlossyButton('clear').setPosition(160,360);
+  // var layer = new lime.Layer();
+  // var board = new dr.Board(320,220);
+  // var submitbtn = dr.makeGlossyButton('submit').setPosition(160,280);
+  // var clearbtn = dr.makeGlossyButton('clear').setPosition(160,360);
 
-  dr.setEvent(submitbtn,'click',function() {
-    board.submit();
-  });
-  dr.setEvent(clearbtn,'click',function() {
-    board.clearBoard();
-  });
+  // dr.setEvent(submitbtn,'click',function() {
+    // board.submit();
+  // });
+  // dr.setEvent(clearbtn,'click',function() {
+    // board.clearBoard();
+  // });
 
-  layer.appendChild(board.canvas);
-  layer.appendChild(submitbtn);
-  layer.appendChild(clearbtn);
-  lib.loadjsfile('assets/ndollar.js',function() {
-    board.init();
-    board.loadAnswers('assets/answers/answer1.js');
-  });
+  // layer.appendChild(board.canvas);
+  // layer.appendChild(submitbtn);
+  // layer.appendChild(clearbtn);
+  // lib.loadjsfile('assets/ndollar.js',function() {
+    // board.init();
+    // board.loadAnswers('assets/answers/answer1.js');
+  // });
 
-  gamescene.appendChild(layer);
+  // gamescene.appendChild(layer);
 
-  // set active scene
+  // // set active scene
+  // dr.director.replaceScene(gamescene);
+  // window.gamescene = gamescene;
+
+  var topBar = new lime.Layer();
+  topBar.setPosition(0,0);
+  tsprite  = new lime.Sprite();
+  tsprite.setFill('#c00').setAnchorPoint(0,0);
+  tsprite.setSize(320,40);
+
+  topBar.appendChild(tsprite);
+
+  gamescene.appendChild(topBar);
+
+
+  var QuizHolder = new lime.Layer().setPosition(0,50);
+  var Quiz = new lime.Sprite().setFill("#D62EDB").setAnchorPoint(0,0);
+  Quiz.setPosition(50,0);
+  Quiz.setSize(220,160);
+
+  qsprite = new lime.Sprite().setFill("#8D2EDB").setAnchorPoint(0,0);
+  qsprite.setSize(320,160);
+
+  QuizHolder.appendChild(qsprite);
+  QuizHolder.appendChild(Quiz);
+
+  gamescene.appendChild(QuizHolder);
+
+
+  var funcBtnHolder = new lime.Layer().setPosition(0,220);
+  fsprite = new lime.Sprite().setFill('#1BE0B5').setAnchorPoint(0,0);
+  fsprite.setSize(320,50);
+
+  funcBtnHolder.appendChild(fsprite);
+
+  gamescene.appendChild(funcBtnHolder);
+
+
+  var Board = new lime.Layer().setPosition(0,280);
+  bsprite = new lime.Sprite().setFill("#E0941B").setAnchorPoint(0,0);
+  bsprite.setSize(320,200);
+
+  Board.appendChild(bsprite);
+
+  gamescene.appendChild(Board);
+
+
   dr.director.replaceScene(gamescene);
-  window.gamescene = gamescene;
 };
 
 dr.isBrokenChrome = function(){
