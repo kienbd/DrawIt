3b427837a7e9cedb1bd17ebffc60f5fbc4bf149e
KienBui <doankien.bui@gmail.com>
2013-03-19 01:07:31 +0700

    fix

--- config.js
+++ config.js
@@ -1,18 +1,8 @@
-// dr.topBar.height = 50;
+goog.provide('dr.config');
 
-// //space = 10
 
-// dr.quizCard.width = 180
-// dr.quizCard.height = 130;
 
-// dr.quizCard.changeButton.width = 20;
-// dr.quizCard.changeButton.height = 20;
+dr.config = function() {};
 
-// //space = 10
 
-// dr.funcButtonsBar.height = 60;
 
-// //scpace = 10
-
-// dr.board.width  = 240;
-// dr.board.height = 160;
